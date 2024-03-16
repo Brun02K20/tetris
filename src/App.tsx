c59bf0646fce075bfa93982ceb6898b1bb6c4e3e
Brun02K23 <bvirinni@gmail.com>
2024-03-16 01:21:36 -0300

    terminando logica y musica

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
 import './App.css'
 import { Board } from './components/Board.tsx'
-import { EmptyCell } from './types.d'
-
-// vamos a primero que todo, crear el pizarron:
-const board = Array(20)
-  .fill(null)
-  .map(() => Array(12).fill(EmptyCell.Empty))
-
+import { useTetris } from './hooks/useTetris.ts'
+import { UpcomingBlocks } from './components/UpcomingBlocks.tsx';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const { board, startGame, isPlaying, score, upcomingBlocks } = useTetris();
+  const [audio] = useState(new Audio('/tetris.mp3'))
+
+  useEffect(() => {
+    if (isPlaying) {
+      audio.play()
+    } else {
+      audio.pause(); // Pausar la música al finalizar el juego
+      audio.currentTime = 0; // Reiniciar la reproducción al principio del archivo
+    }
+  }, [isPlaying, audio])
+
+  useEffect(() => {
+  const handleAudioEnded = () => {
+    if (isPlaying && audio.currentTime === audio.duration) {
+      audio.currentTime = 0; // Reiniciar la reproducción al principio del archivo
+      audio.play();
+    }
+  };
+
+  audio.addEventListener('ended', handleAudioEnded);
+
+  return () => {
+    audio.removeEventListener('ended', handleAudioEnded);
+  };
+}, [isPlaying, audio]);
+
   return (
-    <>
+    <div className='app'>
       <h1>Tetris</h1>
-      {/* Muestro el pizarron, pasandole como pizarron actual, el creado   */}
-      <Board currentBoard={board}/>
-    </>
+      <Board currentBoard={board} />
+      <div className="controls">
+        <h2>Score: {score}</h2>
+        {isPlaying ? (
+          <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
+        ) : (
+          <button onClick={startGame}>New Game</button>
+        )}
+      </div>
+    </div>
   )
 }
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,21 +1,77 @@
-// En el tetris hay 7 tipos de bloques diferentes, por lo tanto, podemos usar una enumeracion
 export enum Block {
-    I = 'I',
-    J = 'J',
-    L = 'L',
-    O = 'O',
-    S = 'S',
-    T = 'T',
-    Z = 'Z',
+  I = 'I',
+  J = 'J',
+  L = 'L',
+  O = 'O',
+  S = 'S',
+  T = 'T',
+  Z = 'Z',
 }
 
-// en el tetris obviamente, vamos a tener celdas vacias
 export enum EmptyCell {
-    Empty = "Empty"
+  Empty = 'Empty',
 }
 
-// una celda puede estar con bloque, o vacia
-export type CellOptions = Block | EmptyCell
+export type CellOptions = Block | EmptyCell;
 
-// defino que mi pizarron, es una matriz de celdas que pueden estar ocupadas o vacias
-export type BoardShape = CellOptions[][]
+export type BoardShape = CellOptions[][];
+
+export type BlockShape = boolean[][];
+
+type ShapesObj = {
+  [key in Block]: {
+    shape: BlockShape;
+  };
+};
+
+export const SHAPES: ShapesObj = {
+  I: {
+    shape: [
+      [false, false, false, false],
+      [false, false, false, false],
+      [true, true, true, true],
+      [false, false, false, false],
+    ],
+  },
+  J: {
+    shape: [
+      [false, false, false],
+      [true, false, false],
+      [true, true, true],
+    ],
+  },
+  L: {
+    shape: [
+      [false, false, false],
+      [false, false, true],
+      [true, true, true],
+    ],
+  },
+  O: {
+    shape: [
+      [true, true],
+      [true, true],
+    ],
+  },
+  S: {
+    shape: [
+      [false, false, false],
+      [false, true, true],
+      [true, true, false],
+    ],
+  },
+  T: {
+    shape: [
+      [false, false, false],
+      [false, true, false],
+      [true, true, true],
+    ],
+  },
+  Z: {
+    shape: [
+      [false, false, false],
+      [true, true, false],
+      [false, true, true],
+    ],
+  },
+};
