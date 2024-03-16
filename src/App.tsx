b561306da7a556e2c91bce8dfafa4ac696382bc0
Brun02K23 <bvirinni@gmail.com>
2024-03-16 02:14:30 -0300

    highscore system

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,25 @@ import '@fontsource/press-start-2p';
 function App() {
   const { board, startGame, isPlaying, score, upcomingBlocks } = useTetris();
   const [audio] = useState(new Audio('/tetris.mp3'))
+  const [highScore, setHighscore] = useState(0)
+
+  const getHighScore = () => {
+    const highScore = Number(window.localStorage.getItem("B2K20Tetris"))
+      if (highScore >= 0) {
+        if (score >= highScore) {
+          window.localStorage.setItem("B2K20Tetris", score.toString())
+          setHighscore(score)
+        } else {
+          setHighscore(highScore)
+        }
+      } else {
+        window.localStorage.setItem("B2K20Tetris", score.toString())
+      }
+  }
+
+  useEffect(() => {
+      getHighScore()
+  }, [isPlaying, score])
 
   useEffect(() => {
     if (isPlaying) {
@@ -38,13 +57,14 @@ function App() {
       <h1 className='text header'>Tetris</h1>
       <Board currentBoard={board} />
       <div className="controls">
+        <h2 className='text score'>HighScore: {highScore}</h2>
         <h2 className='text score'>Score: {score}</h2>
         <br></br>
         <br></br>
         {isPlaying ? (
           <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
         ) : (
-          <button onClick={startGame} className='text button-start'>New Game</button>
+          <button onClick={() => {getHighScore(); startGame()}} className='text button-start'>New Game</button>
         )}
       </div>
     </div>
